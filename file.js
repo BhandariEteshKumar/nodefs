0e05bd68304deb6e828dfe9e0e88fb9e941690ed
Bandari Etesh Kumar <eteshb@gmail.com>
2022-01-10 19:16:57 +0530

    first commit

--- file.js
+++ file.js
@@ -1,24 +1,37 @@
 const fs = require("fs");
 const path = require("path/posix");
-fs.readFile("./awesome.txt", "utf-8", (err, data) => {
-  console.log(data);
-});
 
-const niceQuote = "Make everyday little less ordinary";
-fs.writeFile("./cool.txt", niceQuote, (err) => {
+const date_time = new Date();
+const date = date_time.getDate();
+const time = date_time.getTime();
+
+fs.writeFile(`./backup/${date}-${time}.txt`, "" + date_time, (err) => {
   console.log("completed");
 });
-const [, , num] = process.argv;
-const bulkQuote = "Live More worry less";
-for (let i = 0; i < num; i++) {
-  fs.writeFile(`./backup/text-${i + 1}.html`, bulkQuote, (err) => {
-    console.log("completed");
-  });
-}
+
 fs.readdir("./backup", (err, files) => {
-  for (const file of files) {
-    fs.unlink(path.join("./backup", file), (err) => {
-      console.log("Deleted");
-    });
-  }
+  console.log(files);
 });
+
+// fs.readFile("./awesome.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+// const niceQuote = "Make everyday little less ordinary";
+// fs.writeFile("./cool.txt", niceQuote, (err) => {
+//   console.log("completed");
+// });
+// const [, , num] = process.argv;
+// const bulkQuote = "Live More worry less";
+// for (let i = 0; i < num; i++) {
+//   fs.writeFile(`./backup/text-${i + 1}.html`, bulkQuote, (err) => {
+//     console.log("completed");
+//   });
+// }
+// fs.readdir("./backup", (err, files) => {
+//   for (const file of files) {
+//     fs.unlink(path.join("./backup", file), (err) => {
+//       console.log("Deleted");
+//     });
+//   }
+// });
